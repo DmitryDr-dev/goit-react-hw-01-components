bd55d29450fd386938fdd118c23453e0e51921e2
DmitryDr-dev <dmitrydr.dev@gmail.com>
2021-10-05 16:17:05 +0300

    2nd Task Completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 // data
 import userData from './data/user.json';
+import statisticsData from './data/statistical-data.json';
 
 // components
 import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
 
 function App() {
   return (
-    <Profile
-      imageUrl={userData.avatar}
-      userName={userData.name}
-      tag={userData.tag}
-      location={userData.location}
-      followers={userData.stats.followers}
-      views={userData.stats.views}
-      likes={userData.stats.likes}
-    ></Profile>
+    <div>
+      <Profile
+        imageUrl={userData.avatar}
+        userName={userData.name}
+        tag={userData.tag}
+        location={userData.location}
+        followers={userData.stats.followers}
+        views={userData.stats.views}
+        likes={userData.stats.likes}
+      ></Profile>
+
+      <Statistics items={statisticsData}></Statistics>
+    </div>
   );
 }
 
---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import s from './Statistics.module.css';
+
+function Statistics({ items }) {
+  return (
+    <section className={s.statistics}>
+      <h2 className={s.title}>Upload stats</h2>
+
+      <ul className={s.statsList}>
+        {items.map(({ id, label, percentage }) => (
+          <li key={id} className={s.item}>
+            <span className={s.label}>{label}</span>
+            <span className={s.percentage}>{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+Statistics.propTypes = {
+  id: PropTypes.string,
+  label: PropTypes.string,
+  percentage: PropTypes.number,
+};
+
+export default Statistics;
